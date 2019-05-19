77ca47e4c6ce1ad84d1ed9ec7deeb0532856a395
Roman Bulinski <aboandrus@gmail.com>
2019-05-19 19:09:44 +0200

    Refactor code ( used streams )

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,35 @@
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.stream.Collectors.joining;
+
 public class Main {
 
     static String[] wordsData = {"I", "like", "big", "butts", "and", "I", "cannot", "lie!"};
 
     public static String[] reverse( String[] words ){
 
-        String[][] tempResult = new String[words.length][];
-        String oneWord = "";
         String[] result = new String[words.length];
-        for(int i =0; i<words.length; i++){
-            tempResult[i] = new String[words[i].length()];
-            oneWord = oneWord+words[i];
-        }
-        char[] letters = oneWord.toCharArray();
-        char[] reverseletters = new char[letters.length];
-        int z=0;
-        for(int j=letters.length-1; j>=0; j--){
-            reverseletters[z] = letters[j];
-            z++;
-        }
+        String oneWord = Arrays.stream(words).collect(joining());
+        StringBuilder wordSB = new StringBuilder(oneWord);
+        char[] reverseletters = wordSB.reverse().toString().toCharArray();
 
         int f=0;
         for(int l =0; l<words.length; l++){
             String tempString = "";
             for(int s =0; s<words[l].length(); s++){
-                tempResult[l][s]=String.valueOf(reverseletters[f]);
+                tempString = tempString + reverseletters[f];
                 f++;
-                tempString = tempString + tempResult[l][s];
             }
             result[l] = tempString;
         }
     return result;
     }
 
-
     public static void main(String[] args) {
         String[] res = reverse(wordsData);
         for( int u = 0 ; u<res.length; u++ ){
             System.out.println( res[u]   );
         }
-
-
-
     }
 }
